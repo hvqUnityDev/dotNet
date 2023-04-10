2df16ac131a0d250ec6b7138a86d874e0c18aa34
Trung Anh <trunganh28042003@gmail.com>
2023-04-10 13:05:29 +0700

    fix

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(383, 112);
+            this.button1.Location = new System.Drawing.Point(345, 144);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 0;
