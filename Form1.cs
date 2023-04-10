5fa3beb20984e9a554aed45a5964a4313f154abb
Black Sun <117530589+BLack-Sunn@users.noreply.github.com>
2023-04-10 13:02:41 +0700

    fix form 1

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,16 @@ namespace WinformForIT9
         {
 
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
 
