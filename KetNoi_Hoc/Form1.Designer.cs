972acec5e12a002648aad25826945063e02e6bd8
hvqDevUnity <hoangvanquan2002.untydevelop@gmail.com>
2023-04-10 12:30:56 +0700

    add

---
+++ KetNoi_Hoc/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace KetNoi_Hoc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.txtMaMH = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtTenMH = new System.Windows.Forms.TextBox();
+            this.txtSoTin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.lsvItem = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(94, 29);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Connect";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(694, 666);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(94, 29);
+            this.btnDisconnect.TabIndex = 1;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // txtMaMH
+            // 
+            this.txtMaMH.Location = new System.Drawing.Point(94, 70);
+            this.txtMaMH.Name = "txtMaMH";
+            this.txtMaMH.Size = new System.Drawing.Size(333, 27);
+            this.txtMaMH.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(500, 68);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(94, 29);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtTenMH
+            // 
+            this.txtTenMH.Location = new System.Drawing.Point(261, 177);
+            this.txtTenMH.Name = "txtTenMH";
+            this.txtTenMH.Size = new System.Drawing.Size(333, 27);
+            this.txtTenMH.TabIndex = 4;
+            // 
+            // txtSoTin
+            // 
+            this.txtSoTin.Location = new System.Drawing.Point(261, 210);
+            this.txtSoTin.Name = "txtSoTin";
+            this.txtSoTin.Size = new System.Drawing.Size(333, 27);
+            this.txtSoTin.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "MaMH";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "TenMH";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "So Tin Chi";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(649, 68);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 29);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(649, 103);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(94, 29);
+            this.btnCount.TabIndex = 10;
+            this.btnCount.Text = "Count";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(649, 138);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(94, 29);
+            this.btnShow.TabIndex = 12;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // lsvItem
+            // 
+            this.lsvItem.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lsvItem.FullRowSelect = true;
+            this.lsvItem.Location = new System.Drawing.Point(169, 262);
+            this.lsvItem.Name = "lsvItem";
+            this.lsvItem.Size = new System.Drawing.Size(444, 354);
+            this.lsvItem.TabIndex = 13;
+            this.lsvItem.UseCompatibleStateImageBehavior = false;
+            this.lsvItem.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Mã Môn Học";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tên Môn Học";
+            this.columnHeader2.Width = 200;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Số Tín Chỉ";
+            this.columnHeader3.Width = 120;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 707);
+            this.Controls.Add(this.lsvItem);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSoTin);
+            this.Controls.Add(this.txtTenMH);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtMaMH);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnOpen;
+        private Button btnDisconnect;
+        private TextBox txtMaMH;
+        private Button btnSearch;
+        private TextBox txtTenMH;
+        private TextBox txtSoTin;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button btnAdd;
+        private Button btnCount;
+        private Button btnShow;
+        private ListView lsvItem;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+    }
+}
---
+++ KetNoi_Hoc/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace KetNoi_Hoc
+{
+    public partial class Form1 : Form
+    {
+        // tạo 2 biến cục bộ
+        // chuỗi kết nối
+        string strCon //= @"Data Source=LAPTOP-89GT2MJ0\\KTEAM;Initial Catalog=QLDIEMSV;Integrated Security=True";
+        = @"Data Source=LAPTOP-89GT2MJ0\KTEAM;Initial Catalog=Hoc;Integrated Security=True";
+        SqlConnection sqlCon = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (sqlCon == null)
+                    sqlCon = new SqlConnection(strCon);
+
+                if (sqlCon.State == ConnectionState.Closed)
+                {
+                    sqlCon.Open();
+                    MessageBox.Show("Open Success");
+                }
+
+                 
+
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw;
+            } 
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            sqlCon = null;
+            MessageBox.Show("Disconnect");
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string maMH = txtMaMH.Text;
+            SqlCommand cmd = new SqlCommand();
+
+            // oneline
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText
+                = @"SELECT * FROM dbo.THONGTIN WHERE MaMH = '" + maMH + "'";
+
+            cmd.Connection = sqlCon;
+            //cmd.ExecuteNonQuery(); 
+            // thêm, sửa, xóa dữ liệu -> không trả về kết quả
+
+            //đọc một giá trị
+            //cmd.ExecuteScalar();
+            //Select count () from ThongTin
+
+
+            // đọc dữa liệu
+            SqlDataReader sqlDataReader = cmd.ExecuteReader();
+
+
+            if (sqlDataReader.Read())
+            {
+                string tenMon = sqlDataReader.GetString(1);
+                int tc = sqlDataReader.GetInt32(2);
+
+                txtTenMH.Text = tenMon;
+                txtSoTin.Text = tc.ToString();
+            }
+
+            sqlDataReader.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string maMH = txtMaMH.Text;
+                string tenMh = txtTenMH.Text;
+                string tc = txtTenMH.Text;
+                //int tc = Int32.Parse(txtSoTin.Text);
+
+                SqlCommand cmd = new SqlCommand();
+
+                // oneline
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText
+                = @"INSERT dbo.THONGTIN (MaMH, TenMH, SoTin) VALUES ('" + maMH + "', '" + tenMh + "', " + tc + ")";
+
+                cmd.Connection = sqlCon;
+                cmd.ExecuteNonQuery(); // thêm, sửa, xóa dữ liệu -> không trả về kết quả
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                throw;
+            }
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            // oneline
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText
+            = @"SELECT COUNT(*) FROM dbo.THONGTIN";
+
+            cmd.Connection = sqlCon;
+            int sl = (int)cmd.ExecuteScalar();
+
+            MessageBox.Show(sl.ToString());
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            // oneline
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText
+            = @"Select * FROM dbo.THONGTIN";
+
+            cmd.Connection = sqlCon;
+            SqlDataReader sqlDataReader = cmd.ExecuteReader();
+
+
+            // listview : view = detail
+            // full row select: False
+            while (sqlDataReader.Read())
+            {
+                string maMH = sqlDataReader.GetString(0);
+                string tenMh = sqlDataReader.GetString(1);
+                int tc = sqlDataReader.GetInt32(2);
+                ListViewItem listViewItem= new ListViewItem(maMH);
+                listViewItem.SubItems.Add(tenMh);
+                listViewItem.SubItems.Add(tc.ToString());
+
+                lsvItem.Items.Add(listViewItem);
+            }
+
+            sqlDataReader.Close();
+        }
+    }
+}
